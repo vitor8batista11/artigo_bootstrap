8e691a1909e3757d1c686f2fb156f029da7cdbe5
vitor8batista11 <64660603+vitor8batista11@users.noreply.github.com>
2024-01-28 17:42:43 -0300

    Adicionando função que gera amostras pareadas

--- simulacao_final.R
+++ simulacao_final.R
@@ -151,6 +151,107 @@ simulacao_artigo <- function(distributions = c('normal', 'exponential', 'laplace
   
 }
 
+simulacao_artigo_pareado <- function(distributions = c('normal', 'exponential', 'laplace', 'uniform', 't'),
+                                     ns = c(5, 20, 100), confidence_levels = c(0.8, 0.95, 0.99),
+                                     alphas = c(-1, 0, 1), parameters = c('mean', 'sd'),
+                                     average, variance = 4, B = 100, B_2 = 100, n_sim = 100, seed = 42){
+  
+  df_resultados <- data.frame(intervalo = numeric(),
+                              parametro = character(),
+                              metodo = character(),
+                              distribuicao = character(),
+                              n = numeric(),
+                              confianca = numeric(),
+                              alpha = numeric(),
+                              lim_inf = numeric(),
+                              lim_sup = numeric(),
+                              sucesso = numeric(),
+                              comprimento = numeric())
+  
+  set.seed(seed)
+  
+  for(distribution in distributions) {
+    
+    cat('Distribuição ', distribution, ', ', sep = '')
+    
+    for(n in ns){
+      
+      cat('N = ', n, ', ', sep = '')
+      
+      for(alpha in alphas){
+        
+        cat('Alpha = ', alpha, ' ... ', sep = '')
+        amostras <- replicate(n_sim, generate_sample(distribution, average, variance, alpha, n))
+        
+        for(parameter in parameters){
+          
+          for(confidence_level in confidence_levels){
+            
+            intervalos_bayesian <- apply(amostras, MARGIN = 2, function(amostra) bayesian_interval(amostra, B, confidence_level, parameter)) %>%
+              t() %>% data.frame() %>%
+              mutate(metodo = 'bayesian', intervalo = 1:n_sim)
+            intervalos_better <- apply(amostras, MARGIN = 2, function(amostra) better_interval(amostra, B, confidence_level, parameter)) %>%
+              t() %>% data.frame() %>%
+              mutate(metodo = 'better', intervalo = 1:n_sim)
+            intervalos_quantile <- apply(amostras, MARGIN = 2, function(amostra) quantile_interval(amostra, B, confidence_level, parameter)) %>%
+              t() %>% data.frame() %>%
+              mutate(metodo = 'quantile', intervalo = 1:n_sim)
+            intervalos_normal <- apply(amostras, MARGIN = 2, function(amostra) normal_interval(amostra, B, confidence_level, parameter)) %>%
+              t() %>% data.frame() %>%
+              mutate(metodo = 'normal', intervalo = 1:n_sim)
+            intervalos_studentized <- apply(amostras, MARGIN = 2, function(amostra) boot_t_interval(amostra, B, B_2, confidence_level, parameter)) %>%
+              t() %>% data.frame() %>%
+              mutate(metodo = 'studentized', intervalo = 1:n_sim)
+            
+            resultados <- bind_rows(intervalos_bayesian, intervalos_better, intervalos_quantile,
+                                    intervalos_normal, intervalos_studentized) %>%
+              transmute(intervalo,
+                        metodo,
+                        lim_inf = X1,
+                        lim_sup = X2,
+                        distribuicao = distribution,
+                        parametro = parameter,
+                        n = n,
+                        confianca = confidence_level,
+                        alpha = alpha,
+                        comprimento = X2 - X1)
+            
+            if(parameter == 'mean'){
+              
+              resultados <- resultados %>%
+                mutate(true_value = average) %>%
+                mutate(sucesso = ifelse(between(true_value, lim_inf, lim_sup), 1, 0)) %>%
+                select(-true_value)
+              
+            }else{
+              
+              resultados <- resultados %>%
+                mutate(true_value = sqrt(variance)) %>%
+                mutate(sucesso = ifelse(between(true_value, lim_inf, lim_sup), 1, 0)) %>%
+                select(-true_value)
+              
+            }
+            
+            df_resultados <- bind_rows(df_resultados, resultados)
+            
+            cat('FEITO!\n')
+            
+          }
+          
+        }
+        
+      }
+      
+    }
+    
+  }
+  
+  return(df_resultados)
+  
+}
+
 results <- simulacao_artigo(average = 5, variance = 4, B = 100, B_2 = 100, n_sim = 100)
+results_pareado <- simulacao_artigo_pareado(average = 5, variance = 4, B = 100, B_2 = 100, n_sim = 100)
   
 writexl::write_xlsx(results, "resultados_simulacao.xlsx")
+writexl::write_xlsx(results_pareado, "resultados_simulacao_pareado.xlsx")
